74f8d3efd9e73d6310319d77e541f13c2e8e68ef
petrero <petrero@gmail.com>
2022-05-09 16:51:26 +0300

    13.164(1) Handling the File If you are trying to debug drag and drop operations you need to acces the properties directly in Chrome to see their values.Simply logging the object in general will result in an empty array. Here if we log $event object the dataTransfer.files property will be an emptu array in Chrome even if we drop a files onto element.

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -16,7 +16,7 @@
 				@dragover.prevent.stop="is_dragover = true"
 				@dragenter.prevent.stop="is_dragover = true"
 				@dragleave.prevent.stop="is_dragover = false"
-				@drop.prevent.stop="upload"
+				@drop.prevent.stop="upload($event)"
 			>
 				<h5>Drop your files here</h5>
 			</div>
@@ -52,8 +52,11 @@ export default {
 		}
 	},
 	methods: {
-		upload() {
+		upload($event) {
 			this.is_dragover = false;
+
+			const { files } = $event.dataTransfer;
+			console.log(files);
 		}
 	}
 }
