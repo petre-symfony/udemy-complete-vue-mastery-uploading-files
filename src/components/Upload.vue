57ee6d6e5678fbe540bf568df3f90b0f50c1c30c
petrero <petrero@gmail.com>
2022-05-09 18:43:41 +0300

    13.167 Adding the Progress Bar
    The progress bar wil be always the same color

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -21,22 +21,13 @@
 				<h5>Drop your files here</h5>
 			</div>
 			<hr class="my-6" />
-			<div class="mb-4">
-				<div class="font-bold text-sm">Just another song.mp3</div>
+			<div class="mb-4" v-for="upload in uploads" :key="upload.name">
+				<div class="font-bold text-sm">{{ upload.name }}</div>
 				<div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-					<div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
-				</div>
-			</div>
-			<div class="mb-4">
-				<div class="font-bold text-sm">Just another song.mp3</div>
-				<div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-					<div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-				</div>
-			</div>
-			<div class="mb-4">
-				<div class="font-bold text-sm">Just another song.mp3</div>
-				<div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-					<div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
+					<div class="transition-all progress-bar bg-blue-400"
+						 :class="'bg-blue-400'"
+						 :style="{ width: upload.current_progress + '%'}"
+					></div>
 				</div>
 			</div>
 		</div>
@@ -50,7 +41,8 @@ export default {
 	name: "Upload",
 	data() {
 		return {
-			is_dragover: false
+			is_dragover: false,
+			uploads: []
 		}
 	},
 	methods: {
@@ -66,7 +58,17 @@ export default {
 
 				const storageRef = storage.ref(); //music-7d294.firebaseapp.com - storage bucket in configuration uri firebase.js file
 				const songsRef = storageRef.child(`songs/${file.name}`); ////music-7d294.firebaseapp.com/song/example.mp3
-				songsRef.put(file);
+				const task = songsRef.put(file);
+
+				this.uploads.push({
+					task,
+					current_progress: 0,
+					name: file.name
+				});
+
+				task.on('state_changed', (snapshot) => {
+					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				});
 			});
 
 			console.log(files);
