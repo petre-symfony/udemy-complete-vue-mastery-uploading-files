704644e790d9603da491d9c47702fb3e6e8f288e
petrero <petrero@gmail.com>
2022-05-09 21:04:48 +0300

    13.169 Improving the Progress Bar
    Change the color of the progess bar. Change the color of the filename. Lastly we'll add an icon that will indicate that the upload was a failure or a success in the next git.

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -22,10 +22,12 @@
 			</div>
 			<hr class="my-6" />
 			<div class="mb-4" v-for="upload in uploads" :key="upload.name">
-				<div class="font-bold text-sm">{{ upload.name }}</div>
+				<div class="font-bold text-sm" :class="upload.text_class">
+					<i :class="upload.icon"></i>{{ upload.name }}
+				</div>
 				<div class="flex h-4 overflow-hidden bg-gray-200 rounded">
 					<div class="transition-all progress-bar bg-blue-400"
-						 :class="'bg-blue-400'"
+						 :class="upload.variant"
 						 :style="{ width: upload.current_progress + '%'}"
 					></div>
 				</div>
@@ -63,7 +65,10 @@ export default {
 				const uploadIndex = this.uploads.push({
 					task,
 					current_progress: 0,
-					name: file.name
+					name: file.name,
+					variant: 'bg-blue-400',
+					icon: 'fas fa-spinner fa-spin',
+					text_class: ''
 				}) - 1;
 
 				task.on('state_changed', (snapshot) => {
