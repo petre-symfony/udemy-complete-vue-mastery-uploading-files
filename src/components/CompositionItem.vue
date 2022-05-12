d3e061b2d6e778b023ed0a15e39d5fcc0cdeb07b
petrero <petrero@gmail.com>
2022-05-12 19:23:49 +0300

    13.184(1) Deleting a Song from the Storage/Database
      Delete a song from the storage firebase.
      The firebase storage rules was like so
      rules_version = '2';
      service firebase.storage {
        match /b/{bucket}/o {
          match /{allPaths=**} {
            allow read: if true;
            allow write: if request.auth.uid != null &&
            request.resource.contentType == "audio/mpeg" &&
              request.resource.size < 15 * 1024 * 1024;
          }
        }
      }
      This rules don't permit us to delete a file from storage firebase because delete operation is a write operation and request.resource in this case is empty because we not sending a file, consequently request.resource.contentType and request.resource.size conditions will not pass because contentType and size property will be blank.
      So we must write another rule after write rule:
      allow delete: if request.auth.uid != null;

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -2,7 +2,9 @@
 	<div class="border border-gray-200 p-3 mb-4 rounded">
 		<div v-show="!showForm">
 			<h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
-			<button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+			<button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+				@click.prevent="deleteSong"
+			>
 				<i class="fa fa-times"></i>
 			</button>
 			<button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right" @click.prevent="showForm = !showForm">
@@ -46,7 +48,7 @@
 </template>
 
 <script>
-import { songsCollection } from "@/includes/firebase"
+import { songsCollection, storage } from "@/includes/firebase"
 
 export default {
 	name: "CompositionItem",
@@ -98,6 +100,12 @@ export default {
 			this.in_submission = false;
 			this.alert_variant = 'bg-green-500';
 			this.alert_message = 'Success!';
+		},
+		async deleteSong() {
+			const storageRef = storage.ref();
+			const songRef = storageRef.child(`songs/${this.song.original_name}`);
+
+			await songRef.delete();
 		}
 	}
 }
